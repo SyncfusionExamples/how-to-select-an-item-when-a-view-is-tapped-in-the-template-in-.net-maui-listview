d32ea5b26d49203a96ba1bcf03b999151c7a73c7
AnanthaLakshmiKannan <ananthalakshmi.k@syncfusion.com>
2024-01-18 10:45:20 +0530

    Added sample for How to select an item when a view is tapped in the template.

---
+++ Model/BookInfo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewMaui
+{
+    public class BookInfo : INotifyPropertyChanged
+    {
+        private string bookName;
+        private string bookDescription;
+        private int bookId;
+        private string serialNumber;
+
+        public BookInfo()
+        {
+        }
+
+        public string BookName
+        {
+            get { return bookName; }
+            set
+            {
+                bookName = value;
+                OnPropertyChanged("BookName");
+            }
+        }
+
+
+        public string BookDescription
+        {
+            get { return bookDescription; }
+            set
+            {
+                bookDescription = value;
+                OnPropertyChanged("BookDescription");
+            }
+        }
+
+        public string SerialNumber
+        {
+            get { return this.serialNumber; }
+            set
+            {
+                this.serialNumber = value;
+                OnPropertyChanged("SerialNumber");
+            }
+        }
+
+        public int BookID
+        {
+            get { return this.bookId; }
+            set
+            {
+                this.bookId = value;
+                OnPropertyChanged("BookID");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Syncfusion.Maui.ListView;
+
+namespace ListViewMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void listView_SelectionChanging(object sender, Syncfusion.Maui.ListView.ItemSelectionChangingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+    }
+}
